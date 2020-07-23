8a36ca5ab47d489f40f20a03bfcdc7655134cd8b
Jacob Clevenger <jacobrclevenger@gmail.com>
2020-07-23 17:28:39 -0400

    Bump package version. Expose component metadata to public api. Update build command to publish as flat dir

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@ import './styles/main.scss';
 
 export { KroUIPlugin, KroUIBaseConfigurationPlugin } from './install';
 export { useTheme, useIcons, useDialog, DialogType } from './composables';
+
 export * from './components';
+export * as Metadata from './components/metadata';
